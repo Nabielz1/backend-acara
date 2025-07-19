03f916c39a080303e2c8af3364a0a06c7a392d8b
Nabielz1 <gomblohsyukar@gmail.com>
2025-07-19 22:16:24 +0700

    feat: add dotenv configuration and update database connection logic

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import express from 'express';
 import router from "./routes/api";
-import bodyParser from 'body-parser'; 
+import bodyParser from 'body-parser';
+import db from "./utils/database";
 
-const app = express();
+async function init() {
+    try {
+        const result = await db();
+        console.log("database status:", result);
 
-app.use(bodyParser.json());
+        const app = express();
 
-const PORT = 3000;
+        app.use(bodyParser.json());
 
-app.use('/api', router);
+        const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+        app.use('/api', router);
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+init();
